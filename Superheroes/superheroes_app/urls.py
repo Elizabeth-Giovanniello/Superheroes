308de8fe25fe3d5b8ce94bc273946e3dc52d22a7
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-19 20:19:14 -0500

    Added a page to add superheroes to the database. Tested by adding another hero to the database.:

--- Superheroes/superheroes_app/urls.py
+++ Superheroes/superheroes_app/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'superheroes_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:hero_id>/', views.detail, name='detail')
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create')
 ]
