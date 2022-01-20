56be32ed1d3ee89abd27067e649612c0617b51a2
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-20 16:04:15 -0500

    Created functions and templates to edit and delete heroes from the list.

--- Superheroes/superheroes_app/urls.py
+++ Superheroes/superheroes_app/urls.py
@@ -7,5 +7,7 @@ app_name = 'superheroes_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>/', views.detail, name='detail'),
-    path('new/', views.create, name='create')
+    path('new/', views.create, name='create'),
+    path('<int:id>/edit/', views.edit, name='edit'),
+    path('<int:id>/delete/', views.delete, name='delete')
 ]
--- Superheroes/superheroes_app/views.py
+++ Superheroes/superheroes_app/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -33,3 +34,26 @@ def create(request):
 
     else:
         return render(request, 'superheroes_app/create.html')
+
+def edit(request, id):
+    single_hero = SuperheroApp.objects.get(pk=id)
+    if request.method == "POST":
+        single_hero.name = request.POST.get('name')
+        single_hero.alter_ego = request.POST.get('alter_ego')
+        single_hero.primary_ability = request.POST.get('primary')
+        single_hero.secondary_ability = request.POST.get('secondary')
+        single_hero.catch_phrase = request.POST.get('catchphrase')
+        single_hero.save(update_fields=['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catch_phrase'])
+        return HttpResponseRedirect(reverse('superheroes_app:detail', kwargs={'hero_id': single_hero.id}))
+    else:
+        context = {
+            'single_hero': single_hero
+        }
+        return render(request, 'superheroes_app/edit.html', context)
+
+        #TODO come back and fix the link to bring us back to the details page
+
+def delete(request, id):
+    single_hero = SuperheroApp.objects.get(pk=id)
+    single_hero.delete()
+    return HttpResponseRedirect(reverse('superheroes_app:index'))
